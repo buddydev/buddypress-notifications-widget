f00b566043e40efc615f2ce6417f5bdd2aca2b02
Brajesh Singh <brajesh@cosmiccoders.com>
2013-11-19 04:14:46 +0530

    Change live to on for Js to avoid issue with jQuery 1.9+

--- notification.js
+++ notification.js
@@ -1,22 +1,22 @@
 jQuery(document).ready(function(){
 
     var jq=jQuery;
-    jq(".clear-widget-notifications").live('click',function(){
-        var $this=jq(this);
-        var nonce=get_var_in_url($this.attr('href'),'_wpnonce');
-        $this.text('clearing...');
-        jq.post(ajaxurl,{'action':'bpdev_notification_clear_notifications',
-                         '_wpnonce':nonce,
-                         cookie:encodeURIComponent(document.cookie)
+    jq(document).on('click', '.clear-widget-notifications', function(){
+        var $this = jq( this );
+        var nonce = get_var_in_url( $this.attr( 'href' ), '_wpnonce' );
+        $this.text( 'clearing...' );
+        jq.post( ajaxurl, { 'action':'bpdev_notification_clear_notifications',
+                            '_wpnonce':nonce,
+                            cookie:encodeURIComponent( document.cookie )
 
             
-        },function(resp){
-            if(resp=='1'){
+        }, function( resp ){
+            if( resp == '1' ){
                 //remove notification count
-                jq("#bp-adminbar-notifications-menu").find('span').remove();
-                jq("#bp-adminbar-notifications-menu>ul").remove();
-                jq(".bpnw-notification-list").remove();
-                jq(".notification-count-in-title").text("(0)");
+                jq( '#bp-adminbar-notifications-menu' ).find( 'span' ).remove();
+                jq( '#bp-adminbar-notifications-menu>ul' ).remove();
+                jq( '.bpnw-notification-list' ).remove();
+                jq( '.notification-count-in-title' ).text( '(0)' );
                 //remove all notifications
 
                 $this.remove();//incase someone has used it somewhere else
@@ -28,19 +28,19 @@ jQuery(document).ready(function(){
     });
 
     //for individual notification item
-    jq("#bp-adminbar-notifications-menu span.close-notification").live('click',function(){
+    jq("#bp-adminbar-notifications-menu span.close-notification").live('click', function(){
         var $li=jq(this).parent();
         
 
       return false;
     });
-function get_var_in_url(url,name){
+function get_var_in_url( url, name ){
    // console.log(url);
-    var urla=url.split("?");
-    var qvars=urla[1].split("&");//so we hav an arry of name=val,name=val
-    for(var i=0;i<qvars.length;i++){
-        var qv=qvars[i].split("=");
-        if(qv[0]==name)
+    var urla = url.split( "?" );
+    var qvars = urla[1].split( "&" );//so we hav an arry of name=val,name=val
+    for( var i=0; i < qvars.length; i++ ){
+        var qv = qvars[i].split("=");
+        if( qv[0] == name )
             return qv[1];
       }
       return '';
